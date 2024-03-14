ec66d4178cd4add053a76f138557fa569f55b032
Dagasfi <hiimdanii98@gmail.com>
2024-03-14 17:55:45 +0100

    Update config and utils

--- ui_adapt/utils.py
+++ ui_adapt/utils.py
@@ -9,10 +9,14 @@ from ui_adapt.envs.uidesign import UIDesign
 
 class Config():
 
-    def __init__(self, config_file="config.json") -> None:
-        current_folder = os.path.dirname(os.path.abspath(__file__))
-        config_path = os.path.join(current_folder, config_file)
-        self.config = self.read_config(config_path)
+    def __init__(self, config_data="config.json") -> None:
+
+        if isinstance(config_data, str) and config_data.endswith(".json"):
+            current_folder = os.path.dirname(os.path.abspath(__file__))
+            config_path = os.path.join(current_folder, config_data)
+            self.config = self.read_config(config_path)
+        else:
+            self.config = config_data
 
         # Create Enums for USER, PLATFORM, ENVIRONMENT, and UIDESIGN
         self.user_enums = self.create_enums_from_config(
@@ -67,51 +71,66 @@ def get_actions(config):
     return actions
 
 def get_random_aspect(enum_class, aspect_name):
+    
     if aspect_name in enum_class:
+        possible_values = enum_class[aspect_name]
+        if len(possible_values) == 1:
+            return possible_values(1).name
         random = np.random.choice(enum_class[aspect_name])
         return random.name
     else:
         print(f"Aspect '{aspect_name}' not found in the config.")
         return None
 
-def get_random_ui(config, aspects=[], mode='human'):
+def get_random_ui(config, uiDesignInfo= None, aspects=[], mode='human', client_ws=None,server_ws=None):
     if not aspects:
         aspects = [aspect for aspect in config.uidesign_enums]
     user_interface = {}
     for aspect in aspects:
-        user_interface[aspect.lower()] = get_random_aspect(config.uidesign_enums ,aspect)
+        if uiDesignInfo:
+            user_interface[aspect.lower()] = uiDesignInfo["UIDESIGN"][aspect.lower()]
+        else:
+            user_interface[aspect.lower()] = get_random_aspect(config.uidesign_enums ,aspect)
     combinations = get_combinations(aspects, config.uidesign_enums)
     if mode != 'human':
         return user_interface, combinations
-    return UIDesign(config, user_interface, combinations)
+    return UIDesign(config, user_interface, combinations, client_socket=client_ws, server_socket=server_ws)
 
 
-def get_random_user(config, aspects=[]):
+def get_random_user(config, userinfo= None, aspects=[]):
     if not aspects:
         aspects = [aspect for aspect in config.user_enums]
     user = {}
     for aspect in aspects:
-        user[aspect] = get_random_aspect(config.user_enums ,aspect)
+        if userinfo:
+            user[aspect.lower()] = userinfo["USER"][aspect.lower()]
+        else:
+            user[aspect] = get_random_aspect(config.user_enums ,aspect)
     preferences, combinations_pref = get_random_ui(config, mode='user_preferences')
     combinations = get_combinations(aspects, config.user_enums, combinations_pref)
     return User(config, user, preferences, combinations)
 
 
-def get_random_environment(config, aspects=[]):
+def get_random_environment(config, envinfo= None, aspects=[]):
     if not aspects:
         aspects = [aspect for aspect in config.environment_enums]
-    
     environment = {}
     for aspect in aspects:
-        environment[aspect] = get_random_aspect(config.environment_enums ,aspect)
+        if envinfo:
+            environment[aspect.lower()] = envinfo["ENVIRONMENT"][aspect.lower()]
+        else:
+            environment[aspect] = get_random_aspect(config.environment_enums ,aspect)
     combinations = get_combinations(aspects, config.environment_enums)
     return Environment(config, environment, combinations)
 
-def get_random_platform(config, aspects=[]):
+def get_random_platform(config, platforminfo=None, aspects=[]):
     if not aspects:
         aspects = [aspect for aspect in config.platform_enums]
     platform = {}
     for aspect in aspects:
-        platform[aspect] = get_random_aspect(config.platform_enums ,aspect)
+        if platforminfo:
+            platform[aspect.lower()] = platforminfo["PLATFORM"][aspect.lower()]
+        else:
+            platform[aspect] = get_random_aspect(config.platform_enums ,aspect)
     combinations = get_combinations(aspects, config.platform_enums)
     return Platform(config, platform, combinations)
